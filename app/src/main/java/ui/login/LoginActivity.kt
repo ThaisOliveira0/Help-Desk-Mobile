1cf29fe62a43a7fa7c73d7968d343d0273c0fb21
Thais Oliveira <thatha.gos@gmail.com>
2025-10-05 12:09:03 -0300

    initial commit

---
+++ app/src/main/java/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.projeto_suporte.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projeto_suporte.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
