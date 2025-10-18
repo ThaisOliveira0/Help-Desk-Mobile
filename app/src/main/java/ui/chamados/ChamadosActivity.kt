f9dc2ca025a794ad820d6c4d82cd41a9faac0474
Thais Oliveira <thatha.gos@gmail.com>
2025-10-18 16:49:19 -0300

    feat: Add scope for tickets screen

---
+++ app/src/main/java/ui/chamados/ChamadosActivity.kt
@@ -0,0 +1,12 @@
+package ui.chamados
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projeto_suporte.R
+
+class ChamadosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chamados)
+    }
+}
