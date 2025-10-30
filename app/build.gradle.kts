eb4fd49761fb7abbd40fbd797b873b18c728bd58
Gabriel Soares <gabrsoares@outlook.com>
2025-10-30 17:10:51 -0300

    downgrade project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.projeto_suporte"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.projeto_suporte"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
